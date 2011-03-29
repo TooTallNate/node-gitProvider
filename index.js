271015640405704dc7f7a2a5ad5510602174d7cf
Nathan Rajlich <nathan@tootallnate.net>
2011-03-28 19:56:30 -0700

    More progress. It kinda works as a quick script but the code is currently a mess. Need to modularize and refactor.

--- index.js
+++ index.js
@@ -24,11 +24,10 @@ function setup(repoPath, options) {
   //console.log("Using repository:", repoPath, "isClone:", isClone);
   // Synchronously open the git repo
   var repo = gitteh.openRepository(repoPath);
-  resolveHead(repo, console.log);
 
   return function gitProvider(req, res, next) {
     // Ensure that 'next()' exists (in the case of a plain http.Server)
-    next = next || function() { res.writeHead(404); res.end("Not Found"); };
+    next = next || function(err) { res.writeHead(err ? 500 : 404); res.end(err ? err.stack : "Not Found"); };
 
     if (!req.hasOwnProperty("uri")) { req.uri = url.parse(req.url); }
     if (req.uri.pathname.substring(0, options.mountPoint.length) !== options.mountPoint) return next();
@@ -43,8 +42,76 @@ function setup(repoPath, options) {
     //console.log(parts);
 
     // The first part may be a commit reference, a git tag name, or just simply
-    // the name of the file to server from a top-level.
+    // the name of the file to serve from a top-level.
     var firstPart = parts[0];
+    if (firstPart.length === 40) {
+      // Check the commit id, then git tag, then serve file
+      checkCommit(firstPart);
+    } else {
+      // Check for a git tag, then serve file
+      checkTag(firstPart);
+    }
+
+    function checkCommit(commitRef) {
+      console.log(commitRef);
+      repo.getCommit(commitRef, function(err, commit) {
+        if (err) {
+          if (err.gitError === gitteh.error.GIT_ENOTFOUND) {
+            // first part isn't a git commit, next check for a git tag
+            return checkTag(commitRef);
+          } else {
+            // Some other kind of error, baaddd!
+            return next(err);
+          }
+        }
+        // If there was no error then we got a valid commit id. Serve the file from parts[1]
+        commit.getTree(function(err, tree) {
+          serveGitFile(repo, tree, parts.slice(1), res, next);
+        });
+      });
+    }
+    function checkTag(tag) {
+      listTags(repo, function(err, tags) {
+        if (err) return next(err);
+
+        var tagIndex = tags.indexOf(firstPart);
+        if (tagIndex >= 0) {
+          // Requested a file from a git tag. Resolve the git tag then serve the file from parts[1]
+          resolveTag(repo, tag, function(err, commitRef) {
+            if (err) return next(err);
+            serveCommit(commitRef, parts.slice(1));
+          });
+        } else {
+          // No git tag, all the parts are the path of the file to server.
+          // Serve from HEAD (bare repo) or a real file (cloned repo).
+          if (isClone) {
+            fs.readFile(path.join(repoPath, '..', parts.join('/')), function(err, buf) {
+              if (err) {
+                if (err.code === 'ENOENT') return next();
+                else return next(err);
+              }
+              serveBuffer(buf, res);
+            });
+          } else {
+            // Is a bare repo, serve file from resolved HEAD
+            resolveHead(repo, function(err, commitRef) {
+              if (err) return next(err);
+              serveCommit(commitRef, parts);
+            });
+          }
+        }
+      });
+    }
+
+    function serveCommit(commitRef, parts) {
+      repo.getCommit(commitRef, function(err, commit) {
+        if (err) return next(err);
+        commit.getTree(function(err, tree) {
+          if (err) return next(err);
+          serveGitFile(repo, tree, parts, res, next);
+        });
+      });
+    }
   }
 }
 module.exports = setup;
@@ -59,8 +126,32 @@ setup.DEFAULTS = {
 
 
 // Returns a Buffer of the specified file from the specified repo and commit reference.
-function serveGitFile(repo, commit, filePath, callback) {
+// Recursively gets the tree instances until the last part, which is gets the rawObject
+// for and serves back over HTTP.
+function serveGitFile(repo, tree, parts, res, next) {
+  var thisPart = parts.shift();
+  var isLastPart = parts.length === 0;
+  tree.getEntry(thisPart, function(err, entry) {
+    if (err) return next(err);
+    if (isLastPart) {
+      repo.getRawObject(entry.id, function(err, buf) {
+        if (err) return next(err);
+        if (!buf.data) return next();
+        serveBuffer(buf.data, res);
+      });
+    } else {
+      repo.getTree(entry.id, function(err, entryTree) {
+        if (err) return next(err);
+        serveGitFile(repo, entryTree, parts, res, next);
+      });
+    }
+  });
+}
 
+
+function serveBuffer(buf, res) {
+  res.setHeader('Content-Length', buf.length);
+  res.end(buf);
 }
 
 
